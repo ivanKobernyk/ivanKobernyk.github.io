0e44d846ba066b276cdfbc626b5508f8e85940b4
ivanKobernyk <69421792+ivanKobernyk@users.noreply.github.com>
2020-08-18 23:06:27 +0300

    Add files via upload

--- todoList/main.js
+++ todoList/main.js
@@ -6,11 +6,13 @@ let alert = document.querySelector('.alert');
 todoList.innerHTML = (localStorage.getItem(1)) ? render(JSON.parse(localStorage.getItem(1))) : '';
 let arr = (localStorage.getItem(1)) ? JSON.parse(localStorage.getItem(1)) : [];
 let deleteItem = document.querySelectorAll('.delete');
+let listItem = document.querySelectorAll('.listItem');
 
 listiner();
 
 
 function addelement() {
+    let animate = '';
     let obj = {
         id: arr.length,
         value: input.value,
@@ -19,22 +21,32 @@ function addelement() {
     if (obj.value != '' && obj.value != '\n') {
         arr.push(obj);
         push();
-    } else if (obj.value === '' || obj.value === '\n'){
+    } else if (obj.value === '' || obj.value === '\n') {
         alert.innerHTML = "Enter something, please!";
         setTimeout(() => { alert.innerHTML = '' }, 2000);
         return 0;
     }
     input.value = '';
-    todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)));
+    todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)), 'a', 'animate__slideInDown');
+    listItem = document.querySelectorAll('.listItem');
+    listItem.forEach(el => {
+        if (el.id == arr.length - 1) {
+            el.classList.remove('animate__slideInUp', 'animate__slideInDown');
+            el.classList.add('animate__fadeInDown');
+        };
+    });
+
 
     deleteItem = document.querySelectorAll('.delete');
     listiner();
 }
 
-function render(arr) {
+function render(arr, id = undefined, anim) {
     let key = 0;
     return arr.map((el) => {
-        let str = `<div class='listItem' key=${el.id}>
+        let animate = anim || 'animate__slideInUp';
+        if (el.id >= id) { animate = '' } 
+        let str = `<div class='listItem wow animate__animated ${animate}' id=${el.id}>
         <div class="wrapper">
         <span>${el.time}</span>
         <span class='delete' key=${key}>&#10006;</span></div>
@@ -75,7 +87,6 @@ function removelistiner() {
 function listiner() {
     deleteItem.forEach((el) => {
         el.addEventListener('click', (event) => {
-            console.log(event.target)
             deleteListItem(event.target)
         });
     })
@@ -93,14 +104,19 @@ function deleteListItem(el) {
         key++;
         return obj;
     });
-    push();
 
-    arr = (localStorage.getItem(1)) ? JSON.parse(localStorage.getItem(1)) : [];
-    removelistiner();
 
-    todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)));
+    el.parentNode.parentNode.classList.remove('animate__fadeInDown', 'animate__slideInUp', 'animate__slideInDown');
+    el.parentNode.parentNode.classList.add('animate__fadeOutLeft');
+    setTimeout(() => {
+        push();
+        arr = (localStorage.getItem(1)) ? JSON.parse(localStorage.getItem(1)) : [];
+        removelistiner();
 
-    deleteItem = document.querySelectorAll('.delete');
-    listiner();
+        todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)) ,el.parentNode.parentNode.id);
+
+        deleteItem = document.querySelectorAll('.delete');
+        listiner();
+    }, 500);
 }
 
