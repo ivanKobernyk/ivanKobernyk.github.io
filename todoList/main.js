f7f7c8c4d8950d3cfb26162c8c8c8b59c6b3f09c
ivanKobernyk <69421792+ivanKobernyk@users.noreply.github.com>
2020-08-19 11:17:31 +0300

    Add files via upload

--- todoList/main.js
+++ todoList/main.js
@@ -26,150 +26,182 @@ class Clock {
 }
 /* main */
 
-let btn = document.querySelector('.todoBtn');
-let input = document.querySelector('.todoInput');
-let todoList = document.querySelector('#todoListContainer');
-let alert = document.querySelector('.alert');
-let todo = document.querySelectorAll('.todo');
+document.addEventListener("DOMContentLoaded", function (event) {
 
-todoList.innerHTML = (localStorage.getItem(1)) ? render(JSON.parse(localStorage.getItem(1))) : '';
-let arr = (localStorage.getItem(1)) ? JSON.parse(localStorage.getItem(1)) : [];
-let deleteItem = document.querySelectorAll('.delete');
-let listItem = document.querySelectorAll('.listItem');
 
-let clock = new Clock({template : 'h:m:s'});
+    let btn = document.querySelector('.todoBtn');
+    let input = document.querySelector('.todoInput');
+    let todoList = document.querySelector('#todoListContainer');
+    let alert = document.querySelector('.alert');
+    let todo = document.querySelectorAll('.todo');
 
-listiner();
+    todoList.innerHTML = (localStorage.getItem(1)) ? render(JSON.parse(localStorage.getItem(1))) : '';
+    let arr = (localStorage.getItem(1)) ? JSON.parse(localStorage.getItem(1)) : [];
+    let deleteItem = document.querySelectorAll('.delete');
+    let listItem = document.querySelectorAll('.listItem');
 
-function setoverflow() {
-    todo = document.querySelectorAll('.todo');
-    todo.forEach(el => {
-        if (el.offsetHeight > 399) {
-            el.classList.add('large');
-        }
-    });
-}
-setoverflow();
+    let clock = new Clock({ template: 'h:m:s' });
 
-function addelement() {
-    let obj = {
-        id: arr.length,
-        value: input.value,
-        time: `${clock.render()}`
+    listiner();
+
+    function setoverflow() {
+        todo = document.querySelectorAll('.todo');
+        todo.forEach(el => {
+            if (el.offsetHeight > 399) {
+                el.classList.add('large');
+            }
+        });
     }
-
-    console.log(input.value);
-    if (obj.value == '' || obj.value == '\n') {
-        alert.innerHTML = "Enter something, please!";
-        input.value = null;
-        setTimeout(() => { alert.innerHTML = '' }, 2000);
-        return 0;
-    }
-    else if (obj.value != '' && obj.value != '\n') {
-        arr.push(obj);
-        push();
-    }
-
-    input.value = null;
-    todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)), 'a', 'animate__slideInDown');
-    listItem = document.querySelectorAll('.listItem');
     setoverflow();
 
-    listItem.forEach(el => {
-        if (el.offsetHeight > 599) {
-            el.classList.add('large');
-        }
-        if (el.id == arr.length - 1) {
-            el.classList.remove('animate__slideInUp', 'animate__slideInDown');
-            el.classList.add('animate__fadeInDown');
-        };
-    });
-
-
-    deleteItem = document.querySelectorAll('.delete');
-    listiner();
-}
-
-function render(arr, id = undefined, anim) {
-    let key = 0;
-    return arr.map((el) => {
-        let animate = anim || 'animate__slideInUp';
-        if (el.id >= id) { animate = '' }
-        let str = `<div class='listItem wow animate__faster animate__animated ${animate}' id=${el.id}>
-        <div class="wrapper">
-        <span>${el.time}</span>
-        <span class='delete' key=${key}>&#10006;</span></div>
-        <span class="todo">${el.value}</span>
-        </div>`;
-        key++;
-        return str;
-    }).reverse().join("");
-};
-
-function push() {
-    let json = JSON.stringify(arr);
-    localStorage.setItem(1, json);
-};
-
-
-
-
-
-
-btn.addEventListener("click", () => {
-    addelement();
-});
-
-input.addEventListener('keydown', event => {
-    if (event.shiftKey && (event.key == 'Enter')) {
-    }
-    else if (event.key == 'Enter') {
-        addelement();
-    }
-})
-
-function removelistiner() {
-    deleteItem.forEach((el) => {
-        el.removeEventListener('click', (event) => {
-
-            deleteListItem(event.target)
-        });
-    })
-};
-
-function listiner() {
-    deleteItem.forEach((el) => {
-        el.addEventListener('click', (event) => {
-            deleteListItem(event.target)
-        });
-    })
-};
-
-function deleteListItem(el) {
-    let key = 0;
-    arr.splice(el.getAttribute("key"), 1);
-    arr = arr.map((el) => {
+    function addelement() {
         let obj = {
-            id: key,
-            value: el.value,
-            time: el.time
+            id: arr.length,
+            value: input.value,
+            time: `${clock.render()}`
         }
-        key++;
-        return obj;
-    });
 
+        console.log(input.value);
+        if (obj.value == '' || obj.value == '\n') {
+            alert.innerHTML = "Enter something, please!";
+            input.value = null;
+            setTimeout(() => { alert.innerHTML = '' }, 2000);
+            return 0;
+        }
+        else if (obj.value != '' && obj.value != '\n') {
+            arr.push(obj);
+            push();
+        }
 
-    el.parentNode.parentNode.classList.remove('animate__fadeInDown', 'animate__slideInUp', 'animate__slideInDown');
-    el.parentNode.parentNode.classList.add('animate__fadeOutLeft');
-    setTimeout(() => {
-        push();
-        arr = (localStorage.getItem(1)) ? JSON.parse(localStorage.getItem(1)) : [];
-        removelistiner();
-
-        todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)), el.parentNode.parentNode.id);
+        input.value = null;
+        todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)), 'a', 'animate__slideInDown');
+        listItem = document.querySelectorAll('.listItem');
         setoverflow();
+
+        listItem.forEach(el => {
+            if (el.offsetHeight > 599) {
+                el.classList.add('large');
+            }
+            if (el.id == arr.length - 1) {
+                el.classList.remove('animate__slideInUp', 'animate__slideInDown');
+                el.classList.add('animate__fadeInDown');
+            };
+        });
+
+
         deleteItem = document.querySelectorAll('.delete');
         listiner();
-    }, 200);
-}
+    }
+
+    function render(arr, id = undefined, anim) {
+        let key = 0;
+        return arr.map((el) => {
+            let animate = anim || 'animate__slideInUp';
+            if (el.id >= id) { animate = '' }
+            let str = `<div class='listItem wow animate__faster animate__animated ${animate}' id=${el.id}>
+            <div class="wrapper">
+                 <span>${el.time}</span>
+                 <span class="saved"></span>
+                     <span class='delete' key=${key}>&#10006;</span>
+            </div>
+        <span class="todo" contenteditable="true">${el.value}</span>
+        </div>`;
+            key++;
+            return str;
+        }).reverse().join("");
+    };
+
+    function push() {
+        let json = JSON.stringify(arr);
+        localStorage.setItem(1, json);
+    };
+
+
+
+
+
+
+    btn.addEventListener("click", () => {
+        addelement();
+    });
+
+    input.addEventListener('keydown', event => {
+        if (event.shiftKey && (event.key == 'Enter')) {
+        }
+        else if (event.key == 'Enter') {
+            addelement();
+        }
+    })
+
+    function removelistiner() {
+        deleteItem.forEach((el) => {
+            el.removeEventListener('click', (event) => {
+
+                deleteListItem(event.target)
+            });
+        })
+    };
+
+    function listiner() {
+        deleteItem.forEach((el) => {
+            el.addEventListener('click', (event) => {
+                deleteListItem(event.target)
+            });
+        })
+    };
+
+    function deleteListItem(el) {
+        let key = 0;
+        arr.splice(el.getAttribute("key"), 1);
+        arr = arr.map((el) => {
+            let obj = {
+                id: key,
+                value: el.value,
+                time: el.time
+            }
+            key++;
+            return obj;
+        });
+
+
+        el.parentNode.parentNode.classList.remove('animate__fadeInDown', 'animate__slideInUp', 'animate__slideInDown');
+        el.parentNode.parentNode.classList.add('animate__fadeOutLeft');
+        setTimeout(() => {
+            push();
+            arr = (localStorage.getItem(1)) ? JSON.parse(localStorage.getItem(1)) : [];
+            removelistiner();
+
+            todoList.innerHTML = render(JSON.parse(localStorage.getItem(1)), el.parentNode.parentNode.id);
+            setoverflow();
+            deleteItem = document.querySelectorAll('.delete');
+            listiner();
+        }, 200);
+    }
+    /* edit */
+    function saveNotes(el) {
+        let saved = el.parentNode.querySelector('.saved');
+        if(arr[el.parentNode.id].value == el.innerHTML) return null;
+        arr[el.parentNode.id].value = el.innerHTML;
+        push();
+
+        saved.innerHTML = 'Saved)';
+        el.blur();
+        setTimeout(() => { saved.innerHTML = '' }, 2000);
+    }
+
+    todo.forEach(el => {
+        el.addEventListener('keydown', (event) => {
+            if (event.shiftKey && (event.key == 'Enter')) {
+            }
+            else if (event.key == 'Enter') {
+                saveNotes(el);
+            }
+        });
+        el.addEventListener("blur", (event) => {
+            saveNotes(el);
+        })
+    });
+
+});
+
 
